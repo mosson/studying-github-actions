7ad996e5fb981a849aee87eca1ba8c845668f04a
mosson <cucation@gmail.com>
2021-08-14 17:35:12 +0900

    check depth

--- hoge/fuga/fuga-test.ts
+++ hoge/fuga/fuga-test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
-Deno.test("hoge", () => {
+Deno.test("fuga", () => {
   assertEquals(1 + 1, 2);
 });
--- index-test.ts
+++ index-test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
-Deno.test("1 + 1 = 2", () => {
+Deno.test("root", () => {
   assertEquals(1 + 1, 2);
 });
